1b57719e416d19d57a87e6a245dea5e3b60fefcd
mike239x <mike239x@gmail.com>
2019-05-15 22:13:22 +0200

    Add foundation for the classes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # battleship
-a framework for battleship game
+A framework for battleship game.
+
+# Installing
+Clone the repo and start using it.
+If you use poetry you can also run `poetry install`.
+
+# Game rules
+See [wiki page](https://en.wikipedia.org/wiki/Battleship_(game)).
+The board size is 10 by 10, the ships to be placed are:
+  - 1 ship of length 5
+  - 2 ships of length 4
+  - 3 ships of length 3
+  - 4 ships of length 2
+
--- battleship.py
+++ battleship.py
@@ -1,5 +1,32 @@
-class Server:
-    pass
+__version__ = '0.1.0'
+
+import multiprocessing
+import time
+import zmq
+import numpy as np
 
 class Client:
-    pass
+    def __init__(self):
+        pass
+    def start():
+        pass
+
+class Server:
+    def __init__(self):
+        pass
+    def start():
+        pass
+
+class World:
+    def __init__(self):
+        n = 10
+        self.my_board = np.zeros((n,n))
+        self.opponent_board = np.zeros((n,n))
+
+class Player:
+    # init the board and do whatever else you want to do at the start
+    def init():
+        pass
+    # chose your next move given the current state
+    def make_a_move():
+        pass
