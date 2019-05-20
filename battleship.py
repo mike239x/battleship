1d3c0f2182a7e8223dd57b7d888829b346b3fbc2
mikhail.lezhnin <mikhail.lezhnin@esrlabs.com>
2019-05-20 17:30:04 +0200

    Implement most of the server logic using ZMQ
    The very start with connecting players and getting their ship placements
    is lacking right now, as well as port generation.

--- battleship.py
+++ battleship.py
@@ -1,6 +1,7 @@
 __version__ = '0.1.0'
 
-import multiprocessing
+from multiprocessing import Process
+import pickle
 import time
 import zmq
 import numpy as np
@@ -57,6 +58,8 @@ class ActionResult(Enum):
     HIT = 1
     SUNK = 2
 
+CONFIRMED = b'Okay'
+
 # probes a position `pos` of the opponent's `field`.
 # previous probed positions are provided in by `probed`.
 # returns ActionResult
@@ -106,28 +109,66 @@ class Server:
         self.players = (Player(), Player())
         self.allow_illegal_moves = allow_illegal_moves
         self.allow_repeating_moves = allow_repeating_moves
+
     def start(self):
         # TODO get both players connected and get their ships dispositions
+        port1 = "tcp://127.0.0.1:5555"
+        port2 = "tcp://127.0.0.1:5556"
         ...
-        # main game loop:
+        context = zmq.Context()
+        self.sockets = []
+        sock = context.socket(zmq.PAIR)
+        sock.bind(port1)
+        self.sockets.append(sock)
+        sock = context.socket(zmq.PAIR)
+        sock.bind(port2)
+        self.sockets.append(sock)
+        self.process = Process(target = self.run, args = ())
+        self.process.start()
+
+    def force_stop(self):
+        self.process.terminate()
+
+    def announce_winner(self, winner):
+        # write to the winner
+        self.sockets[winner].send_string("You won")
+        # get response
+        msg = self.sockets[winner].recv()
+        assert(msg == CONFIRMED)
+        # write to the loser
+        self.sockets[1-winner].send_string("You lost")
+        # get response
+        msg = self.sockets[1-winner].recv()
+        assert(msg == CONFIRMED)
+
+    # main game loop:
+    def run(self):
         cur = 0 # id of the active player
-        while True:
-            # TODO get a move from a current player and store it in (x,y)
-            pos = (0,0)
+        finished = False
+        while not finished:
+            # ask the active player for a move:
+            self.sockets[cur].send_string("Your turn")
+            # TODO give it X seconds for thinking
+            # get its response
+            msg = self.sockets[cur].recv()
+            pos = pickle.loads(msg)
             x,y = pos
             re = probe(pos, self.players[cur].probed, self.players[1-cur].field)
+            # reply to the player of the action result
+            self.sockets[cur].send(pickle.dumps(re))
+            # get comfirmation
+            msg = self.sockets[cur].recv()
+            assert (msg == CONFIRMED)
             if re == ActionResult.ILLEGAL_MOVE:
-                if allow_illegal_moves:
-                    cur = 1 - cur
-                else:
-                    # TODO kill the active player
-                    ...
+                cur = 1 - cur
+                if not self.allow_illegal_moves:
+                    self.announce_winner(1-cur)
+                    finished = True
             elif re == ActionResult.REPEATING_MOVE:
-                if allow_repeating_moves:
-                    cur = 1 - cur
-                else:
-                    # TODO kill the active player
-                    ...
+                cur = 1 - cur
+                if not self.allow_repeating_moves:
+                    self.announce_winner(1-cur)
+                    finished = True
             elif re == ActionResult.MISS:
                 self.players[cur].probed[y][x] = True
                 cur = 1 - cur
@@ -136,12 +177,11 @@ class Server:
             elif re == ActionResult.SUNK:
                 self.players[cur].probed[y][x] = True
                 if all(self.players[cur].probed[self.players[1-cur].field > 0]):
-                    # TODO active player won
-                    ...
-            # TODO broadcast updates
-            ...
+                    self.announce_winner(cur)
+                    finished = True
+                    # TODO shall we play till the end after the first one finished?
+            # TODO broadcast updates into outer space?
         # end of the main game loop
-        # TODO gracefully shutdown? or keep going?
 
 class Agent:
     # init the board and do whatever else you want to do at the start
--- unittests/test_server.py
+++ unittests/test_server.py
@@ -2,4 +2,7 @@ from battleship import *
 
 def test_0():
     s = Server()
+    s.start()
+    time.sleep(0.1)
+    s.force_stop()
 
