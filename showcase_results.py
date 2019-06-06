38948ba55d487c84b014a70df8d9f21f9f7bec99
Ilya Shpigor <petrsum@gmail.com>
2019-06-06 10:39:47 +0200

    Show 3x3 battle in demo script

--- showcase_results.py
+++ showcase_results.py
@@ -3,9 +3,9 @@ from Ui import run_in_qt, run_on_console
 
 agent1 = SuperAgent(load_random_ships())
 agent1.model_save('superagent_weights')
-agent1.exploration_rate = 0.1
 
-agent2 = SmartAgent(load_random_ships())
+agent2 = SuperAgent(load_random_ships())
+agent2.model_save('superagent_weights')
 
 soft_rules = DefaultRules()
 soft_rules.illegal_moves.pop()
