d4d5f4d5e75555ea0566667ae29d4f75329ec2b9
Ilya Shpigor <petrsum@gmail.com>
2019-06-06 10:46:26 +0200

    Fix the bug with loading model in the showcase_results.py scipt

--- showcase_results.py
+++ showcase_results.py
@@ -2,7 +2,7 @@ from battleship import *
 from Ui import run_in_qt, run_on_console
 
 agent1 = SuperAgent(load_random_ships())
-agent1.model_save('superagent_weights')
+agent1.model_load('superagent_weights')
 agent1.exploration_rate = 0.1
 
 agent2 = SmartAgent(load_random_ships())
